2c2d94253c6d7e2872b498e8ff8903bac7a214ed
M Mahrous <m.mahrous@thed.io>
2017-07-05 20:01:36 +0300

    change the git file

--- func.js
+++ func.js
@@ -1,5 +1,5 @@
 'use strict';
-const git = require('simple-git')('./files')
+const git = require('simple-git')('files')
 				.addConfig('user.name', process.env.gituser || 'swagger')
         		.addConfig('user.email', process.env.gitemail || 'swagger@swagger.com');
 const func = {}
