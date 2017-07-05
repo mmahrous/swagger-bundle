8d6f56d481347629e02eba951bf88cc81f815043
M Mahrous <m.mahrous@thed.io>
2017-07-05 19:41:10 +0300

    add specs file versioning

---
+++ func.js
@@ -0,0 +1,32 @@
+'use strict';
+const git = require('simple-git')('./files')
+				.addConfig('user.name', process.env.gituser || 'swagger')
+        		.addConfig('user.email', process.env.gitemail || 'swagger@swagger.com');
+const func = {}
+
+func.git = {}
+
+func.git.init = () => {
+	git.init(null, (err, done) => {
+		if (!err) console.log("Git init for files");
+	})	
+}
+
+func.git.commit = () => {
+	git.add('./*')
+        .commit(`${new Date().toString()} commit`);
+    console.log(`${new Date().toString()} commit`);
+}
+
+func.git.log = (callback) => {
+	git.log([], (err, logs) => {
+		callback(err, logs)
+	})	
+}
+
+func.git.revert = (hash, callback) => {
+	git.revert(hash, [], (err) => {
+		callback(err)
+	})
+}
+module.exports = func;
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const express = require('express');
 const concat = require('concat-stream');
 const morgan = require('morgan');
+const querystring = require('querystring');
+const _ = require('lodash');
+const func = require('./func');
 
 const app = express()
 
@@ -25,16 +28,40 @@ app.get('/editor/spec', function (req, res) {
 
 app.put('/editor/spec', function (req, res) {
 	fs.writeFile('./files/spec.yaml', req.body, {encoding: 'utf8'}, (err) => {
-		if (!err) return res.send(req.body.toString());
+		if (!err) {
+      // commit file changes
+      func.git.commit();
+      return res.send(req.body.toString());
+    }
 	})
 })
 
+// File versioing 
+app.use('/versions',  express.static('public'));
 // Swagger Editor
 app.use('/editor', express.static('swagger-editor'));
 // Swager UI
 app.use('/', express.static('swagger-ui/dist'));
 
+
+
+// Git 
+app.post('/specs/git/list', function (req, res) {
+  var query = querystring.parse(req.body.toString(), null, null,{});
+   func.git.log( (err, logs) => {
+    if (!err) return res.send({current: query.current,rowCount: query.rowCount,rows: _.chunk(logs.all, query.rowCount)[parseInt(query.current)-1], total: logs.all.length})
+   })
+})  
+
+app.post('/specs/git/revert', function (req, res) {
+  func.git.revert(req.body.toString(), (err) => {
+    if (!err) return res.send(`${req.body.toString()} reverted.`)
+  })
+})  
+
 app.listen(process.env.PORT || 8080, function () {
+  // init a git repo
+  func.git.init();
   console.log('Swagger server running :D');
   console.log('Happy documenting');
 })
