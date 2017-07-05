daddcebc6f079d06f19cd1d47d2d736501ec870e
M Mahrous <m.mahrous@thed.io>
2017-07-05 16:00:50 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 !#/bin/bash
 
-#wget https://github.com/swagger-api/swagger-editor/releases/download/v2.10.4/swagger-editor.zip
-#unzip swagger-editor.zip
+# Fetch swagger editor files
+wget https://github.com/swagger-api/swagger-editor/releases/download/v2.10.4/swagger-editor.zip
+unzip swagger-editor.zip
 
+# Fetch swager UI files
 git clone https://github.com/swagger-api/swagger-ui.git
 
+# npm
 npm install
 
-sed '/    url: "http://petstore.swagger.io/v2/swagger.json",/c     url: "http://127.0.0.1/editor/spec",' swagger-ui/dist/index.html
+# Replace swagger ui API url with the local one
+sed -i "s%http://petstore.swagger.io/v2/swagger.json%/editor/spec%g" "swagger-ui/dist/index.html"
+
+echo "Done building happy documenting :D"
